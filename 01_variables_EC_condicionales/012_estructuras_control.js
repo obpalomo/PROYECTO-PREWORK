2fccf26d9f36ea797e8c472f9d2d02a22cae85af
Pedro Gonzalez <dev3.pgonzalez@gmail.com>
2023-10-19 21:06:44 +0200

    ejercicios

--- 01_variables_EC_condicionales/012_estructuras_control.js
+++ 01_variables_EC_condicionales/012_estructuras_control.js
@@ -1,12 +1,12 @@
 /* 1. OPERADORES LÓGICOS (OR, AND, NOT) */
 
 /* or */
-let llueve = false;
+let llueve = true;
 let fregada = false;
 
 let isSueloMojado = llueve || fregada;
 
-console.log('mojado', isSueloMojado);
+console.log('mojado', isSueloMojado); // true
 
 
 /* and */
@@ -42,13 +42,29 @@ let multi = x * y;
 let divi = x / y;
 let modulo = x%y;
 
-//let incremento = x--;
+/* pre-incremento ++x -> se incrementa la variable y luego se asigna */
 
-/* ++x; */
-/* x++; */
+let pre = ++x; // pre = 16, x = 16
 
-let z = ++x;
+/* post-incremento x++ -> se asigna la variable y luego se incrementa */
 
-console.log(z, x);
+let post = y++; // pre = 4, y = 5
 
+
+/* 
+EJERCICIOS
+
+1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.
+
+1.2 Divide 10 por 2 y muestra el resultado en un alert.
+
+1.3 Muestra mediante un alert el resto de dividir 15 por 9.
+
+1.4 Usa el correcto operador de asignación que resultará en x = 15, 
+teniendo dos variables y = 10 y z = 5.
+
+1.5 Usa el correcto operador de asignación que resultará en x = 50,
+teniendo dos variables y = 10 y z = 5.
+
+*/
 
