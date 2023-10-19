f47ae4edf0da95bdc9bc133407bc373b3e308f59
Pedro Gonzalez <dev3.pgonzalez@gmail.com>
2023-10-19 21:19:01 +0200

    ejercicio 2

--- 01_variables_EC_condicionales/012_estructuras_control.js
+++ 01_variables_EC_condicionales/012_estructuras_control.js
@@ -54,11 +54,11 @@ let post = y++; // pre = 4, y = 5
 /* 
 EJERCICIOS
 
-1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.
+1.1 Multiplica 10 por 5 y muestra el resultado mediante console.log.
 
-1.2 Divide 10 por 2 y muestra el resultado en un alert.
+1.2 Divide 10 por 2 y muestra el resultado en un console.log.
 
-1.3 Muestra mediante un alert el resto de dividir 15 por 9.
+1.3 Muestra mediante un console.log el resto de dividir 15 por 9.
 
 1.4 Usa el correcto operador de asignación que resultará en x = 15, 
 teniendo dos variables y = 10 y z = 5.
