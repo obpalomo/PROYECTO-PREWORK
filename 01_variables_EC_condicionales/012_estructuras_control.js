bfa38765dd8c0613ebf4fea2d1afc922af43819c
Pedro Gonzalez <dev3.pgonzalez@gmail.com>
2023-10-19 20:43:13 +0200

    hasta condicionales

--- 01_variables_EC_condicionales/012_estructuras_control.js
+++ 01_variables_EC_condicionales/012_estructuras_control.js
@@ -1,4 +1,54 @@
 /* 1. OPERADORES LÓGICOS (OR, AND, NOT) */
 
+/* or */
+let llueve = false;
+let fregada = false;
+
+let isSueloMojado = llueve || fregada;
+
+console.log('mojado', isSueloMojado);
+
+
+/* and */
+let soyRico = false;
+let quieroUnFerrari = true;
+
+let vasATenerUnFerrari = soyRico && quieroUnFerrari;
+
+console.log('tengo', vasATenerUnFerrari);
+
+/* not */
+
+let tengoDinero = false;
+
+if (tengoDinero) {
+    console.log('no ir al cajero');
+} else {
+    console.log('ir al cajero');
+}
+
+/* !true -> false
+!false -> true */
+
 /* 2. OPERADORES ARITMÉTICOS (+, -, *, /, %, ++, --) */
+
+let x = 15
+let y = 4
+
+// aritmetica
+let suma = x + y;
+let resta = x - y;
+let multi = x * y;
+let divi = x / y;
+let modulo = x%y;
+
+//let incremento = x--;
+
+/* ++x; */
+/* x++; */
+
+let z = ++x;
+
+console.log(z, x);
+
 
--- 01_variables_EC_condicionales/013_condicionales.js
+++ 01_variables_EC_condicionales/013_condicionales.js
@@ -1,5 +1,79 @@
 /* 1. IF */
-/* HOLA */
+
+let llueve = false;
+
+/* 
+let cerrarVentana = false;
+if (llueve) {
+    cerrarVentana = true; 
+} 
+
+*/
+
+/* dfsfd */
+
 /* 2. IF - ELSE */
 
-/* 3. TERNARIO */
+let cerrarVentana;
+if (llueve) {
+    cerrarVentana = true;
+} else {
+    cerrarVentana = false;
+}
+
+/* 3. IF-ELSE */
+
+let soldados = 100;
+let orcos = 200;
+
+if (soldados > orcos) {
+    alert('atacar');
+} else if (soldados == orcos) {
+    alert('defender');
+} else if (soldados < orcos) {
+    alert('pedir refuerzos');
+} else {
+    alert('salir corriendo');
+}
+
+/* . TERNARIO */
+
+
+
+/* EJERCICIOS */
+
+/* 
+const number1 = 10;
+const number2 = 20;
+const number3 = 2;
+
+// ejemplo
+if(number1 === 10){
+    console.log('number1 es estrictamente igual a 10')
+}
+
+if (COMPLETAR) {
+    console.log("number2 dividido entre number1 es igual a 2");
+}
+
+if (COMPLETAR) {
+  console.log("number1 es estrictamente distinto a number2");
+}
+
+if (COMPLETAR) {
+  console.log("number3 es distinto number1");
+}
+
+if (COMPLETAR) {
+  console.log("number3 por 5 es igual a number1");
+}
+
+if (COMPLETAR) {
+  console.log("number3 por 5 es igual a number1 Y number1 por 2 es igual a number2");
+}
+
+if (COMPLETAR) {
+  console.log("number2 entre 2 es igual a number1 O number1 entre 5 es igual a number3");
+}
+
+*/
