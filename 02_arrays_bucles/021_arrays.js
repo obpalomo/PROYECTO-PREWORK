7ea79b4d865371acb7465e8cb12765c00735bcb1
Pedro Gonzalez <dev3.pgonzalez@gmail.com>
2023-10-21 10:22:02 +0200

    bucles

--- 02_arrays_bucles/021_arrays.js
+++ 02_arrays_bucles/021_arrays.js
@@ -25,12 +25,28 @@ myArrayDeArrays = [
     [1, 3, 4, 5]
 ];
 myArrayTipoMatriz = [
-    [1, 5, 6],
-    [2, 4, 8],
-    [3, 6, 7]
+    [11, 5, 6],
+    [33, 9, 8],
+    [22, 12, 7]
 ];
 
+
+
+console.log(myArrayTipoMatriz[0][1]);
+
 //EJERCICIO PRACTICO: crea un array de tres dimensiones y longitud 2 en cada dimension, usa tipos numericos, saca por consola la suma de las posiciones 0-1-1 y 1-1-0.
+
+let array3Dimensiones = [
+    [
+        [1],[1],[1]
+    ],
+    [
+        [1],[1],[1]
+    ],
+    [
+        [1],[1],[1]
+    ],
+]
 
 
 // 1.3 ACCESO A ELEMENTOS POR ÍNDICE.
@@ -45,32 +61,105 @@ myArray[0] = 'adiós';
 /* 2. MÉTODOS CON ARRAYS */
 
 // 2.1 POP / SHIFT -> elimina el último / primer elemento del array.
+let myArray2 = ['hola', 'hello', 'hallo'];
+
+/* myArray2.pop();
+
+myArray2.shift(); */
+
 
 // 2.2 PUSH -> añade un elemento al final.
 
+myArray2.push('ciao');
+
 // 2.3 REVERSE -> invierte el orden del array
 
+myArray2.reverse();
+
+console.log('reverse', myArray2);
+
 // 2.4 SORT -> ordena el array (a-z strings, creciente en numericos)
+
+let a = myArrayTipoMatriz[0].sort();
+console.log('sort',a);
 
 // 2.5 SPLICE
 // splice(n) -> elimina n elementos partiendo de 0 
 // splice(n, m) -> elimina n elementos partiendo del índice m
 // splice(n, m, args) -> elimina n elementos partiendo del indice m y después inserta los definidos en args
 
+let splic = [1,2,3,4,5,6,7,8];
+
+//let splic1 = splic.splice(3);
+//console.log(splic1);
+
+/* let splic2 = splic.splice(2, 2);
+console.log(splic2); */
+/* 
+let splic3 = ; */
+console.log('3', splic.splice(0, 2, 'test'));
+
 // EJERCICIO PRÁCTICO: dado el array ['juan', 'ramón', 'julio', 'ana', 'beatriz'], inserta dos nombres de tu elección entre julio y ana.
 
 // 2.6 UNSHIT -> inserta n elementos al principio del array
 
+splic.unshift(1,1,1);
+
+
 // 2.7 CONCAT -> une dos arrays, hay que almacenar el valor en una nueva variable
+
+let array1 = [1,2,3];
+let array2 = [4,5,6];
+let array3 = [8,5,6];
+let array4 = [9,5,6];
+
+let arrayConcat = array2.concat(array1).concat(array3).concat(array4);
+
+console.log('concat', arrayConcat);
+
 
 // 2.8 JOIN -> convierte los valores del array en un string separado por comas o por el parámetro indicado.
 
+let arrayJoin = arrayConcat.join('//');
+console.log('join',arrayJoin);
+
 // 2.9 TOSTRING 
 
+let arrayString = arrayConcat.toString();
+console.log('tostr', arrayString);
+
 // 2.10 INDEXOF, LASTINDEXOF
+//['hola', 'hello', 'hallo', 'hallo', 'hallo'];
+console.log('indexOf', myArray.indexOf('hello'));
+myArray = ['hola', 'hello', 'hallo', 'hallo', 'hallo'];
+console.log('lastindexOf', myArray.lastIndexOf('hallo'));
 
 // 2.11 INCLUDES
 
+if (myArray.includes('hola')) {
+    console.log('incluye hola');
+}
 
 // HACER LOS EJERCIOS PROPUESTOS
 
+/* 
+1.1 Consigue el valor "HULK" del array de cars y muestralo por consola.
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+
+1.2 Cambia el primer elemento de avengers a "IRONMAN"
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+
+1.3 Alert numero de elementos en el array usando la propiedad correcta de Array.
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+
+1.4 Añade 2 elementos al array: "Morty" y "Summer". 
+Muestra en consola el último personaje del array
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+
+1.5 Elimina el último elemento del array y muestra el primero y el último por consola.
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+
+1.6 Elimina el segundo elemento del array y muestra el array por consola.
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+
+*/
--- 02_arrays_bucles/023_blucles.js
+++ 02_arrays_bucles/023_blucles.js
@@ -2,12 +2,67 @@
 
 /* 1. FOR */
 
+var arrayCoches = ['ferrari', 'audi', 'seat', 'dacia', 'ford', 'chevrolet', 'toyota', 'kia'];
+var arrayCorregido = new Array();
+
+
+/* let gato = {nombre: 'honey', edad: 1};
+let gato2 = {edad: 1, nombre: 'honey'}; */
+
+
+// console.log('test',arrayCoches[0][0]);
+
+/* for (let i = 0; i < arrayCoches.length; i++) {
+    console.log(arrayCoches[i]);
+} */
+
+
 /* 1B. FOREACH */
+
+/* arrayCoches.forEach(element => {
+    arrayCorregido.push(element.toUpperCase());
+}); */
+
+//console.log(arrayCorregido);
 
 
 
 
 /* 2. OTROS BUCLES: while, do while,  */
+
+let day = 21;
+/* while (day < 30 && day > 20) {
+    console.log('es final de mes');
+    day++;
+} */
+
+let colaDeGente = 10;
+/* do {
+    console.log('hola');
+    colaDeGente--;
+} while(colaDeGente > 0); */
+
+let meses = ['marzo', 'abril', 'mayo'];
+/* switch(meses) {
+    case 'marzo':
+        console.log('llevar chaquetilla');
+        break;
+    case 'abril':
+        console.log('llevar sudadera');
+        break;
+    case 'mayo':
+        console.log('llevar gorra');
+        break;
+    default:
+        console.log('has usado el array que no era');
+        break;
+} */
+
+/* let mesesObj = {
+    marzo: 'llevar chaquetilla',
+    abril: 'llevar sudadera',
+}
+ */
 
 /* 3A. FOR OF */
 
@@ -16,20 +71,49 @@
 
 // EJERCICIOS:
 // a: hacer el método .includes con un bucle, testarlo con:
-let isIncluded('ana');
-let names = ['juan', 'ramón', 'julio', 'ana', 'beatriz']
+
+var arrayCoches2 = ['ferrari', 'audi', 'seat', 'dacia', 'bmw', 'chevrolet', 'toyota', 'kia'];
+
+let isBmw;
+for (let i = 0; i < arrayCoches2.length; i++) {
+    if(arrayCoches2[i] == 'bmw') {
+        isBmw = true;
+        break;
+    } 
+    if (i == arrayCoches2.length -1 && !isBmw) {
+        isBmw = false;
+    }
+} 
+console.log('for bmw', isBmw);
+
+
+if (arrayCoches2.includes('ferrari')) {
+    console.log('ferrari');
+} else if (arrayCoches2.includes('audi')) {
+    console.log('audi');
+} else if (arrayCoches2.includes('alfa romeo')) {
+    console.log('alfa romeo');
+} else if (arrayCoches2.includes('bmw')) {
+    console.log('bmw');
+}
+
+
+/* let isIncluded = ('ana');
+let names = ['juan', 'ramón', 'julio', 'ana', 'beatriz'] */
 // b: dado el array ['juan', 'ramón', 'julio', 'ana', 'beatriz'], añadir un nombre nuevo detras de cada uno existente usando bucles.
 
+
+
 // c: averigua si el número 2 existe en la siguiente matriz usando solo bucles:
-let matriz = [
+/* let matriz = [
     [1,2,4],
     [2,3,4],
     [5,6,7]
-]
+] */
 // d: saca por cada consola el sumatorio de cada array dentro de matriz, usa solo bucles.
 
 // e: dados los siguentes objetos tipo coche:
-let cocheA = {marca: 'seat', matriculacion: 2008, etiqueta: '?'};
+/* let cocheA = {marca: 'seat', matriculacion: 2008, etiqueta: '?'};
 let cocheB = {marca: 'alfa romeo', matriculacion: 2000, etiqueta: '?'};
-let cocheC = {marca: 'bmw', matriculacion: 2019, etiqueta: '?'};
+let cocheC = {marca: 'bmw', matriculacion: 2019, etiqueta: '?'}; */
 // guarda todos los coches en un array y, mediante bucles, itéralo para asignar a cada coche su etiqueta corresponciente (<2006 -> A, 2006 - 2012 -> B, >2012 -> C)
